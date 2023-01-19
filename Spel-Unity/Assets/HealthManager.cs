1e7e15c815cac1a70898cf5f90ae3a58eba2cbff
Rastmust <rasmus.wiklund123@gmail.com>
2023-01-19 14:31:24 +0100

    smaller changes

--- Spel-Unity/Assets/HealthManager.cs
+++ Spel-Unity/Assets/HealthManager.cs
@@ -6,12 +6,14 @@ public class HealthManager : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
+
+
     public void HurtPlayer(int damageToGive)
     {
         currentHealth -= damageToGive;
         if (currentHealth <= 0)
         {
-            gameObject.SetActive(false);
+            gameObject.Transform.Position = spawnPoint.Transform.Position
         }
     }
 }
